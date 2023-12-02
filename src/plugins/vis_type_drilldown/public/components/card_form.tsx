abde400ddc935a9459519861dcf22fca84443333
shawn <shawnkengkiat@gmail.com>
2023-12-01 22:43:25 -0800

    refactor: implements EuiForms

--- src/plugins/vis_type_drilldown/public/components/card_form.tsx
+++ src/plugins/vis_type_drilldown/public/components/card_form.tsx
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import {
   EuiPanel,
   EuiTitle,
@@ -13,10 +13,12 @@ import {
   EuiAccordion,
   EuiFlexGroup,
   EuiSuperSelect,
+  EuiForm,
+  EuiFormRow,
+  EuiRadioGroup,
 } from '@elastic/eui';
 import { i18n } from '@osd/i18n';
 import { Card } from '../types';
-
 interface CardFormProps {
   index: number;
   card: Card;
@@ -34,6 +36,24 @@ const CardForm = ({
   valueOfSelected,
   onChange,
 }: CardFormProps) => {
+  // Variables for radio buttons
+  const [radioIdSelected, setRadioIdSelected] = useState('test2');
+
+  const radioChange = (optionId: string) => {
+    setRadioIdSelected(optionId);
+  };
+
+  const radios = [
+    {
+      id: 'test1',
+      label: 'Go to URL',
+    },
+    {
+      id: 'test2',
+      label: 'Go to dashboard',
+    },
+  ];
+
   return (
     <EuiAccordion
       id={String(index)}
@@ -42,22 +62,10 @@ const CardForm = ({
       initialIsOpen={true}
     >
       <EuiPanel paddingSize="s">
-        <EuiFlexGroup direction="column" gutterSize="m" className="eui-fullHeight">
-          <EuiFlexItem>
-            <EuiTitle size="xs">
-              <h2>
-                <label
-                  htmlFor={i18n.translate('drilldownVisInput', {
-                    defaultMessage: 'Drilldown Visualisation CardName',
-                  })}
-                >
-                  Card Name
-                </label>
-              </h2>
-            </EuiTitle>
-          </EuiFlexItem>
-
-          <EuiFlexItem>
+        {/* TODO: @ShawnkChan Logic for invalid form */}
+        <EuiForm>
+          {/* Name of Drilldown configuration */}
+          <EuiFormRow label="Card Name" className="">
             <EuiFieldText
               id="drilldownVisInput"
               className="eui-fullHeight"
@@ -67,84 +75,124 @@ const CardForm = ({
               }}
               fullWidth={true}
             />
-          </EuiFlexItem>
-
-          <EuiFlexItem>
-            <EuiTitle size="xs">
-              <h2>
-                <label
-                  htmlFor={i18n.translate('drilldownVisInput', {
-                    defaultMessage: 'Drilldown Visualisation Description',
-                  })}
-                >
-                  Description
-                </label>
-              </h2>
-            </EuiTitle>
-          </EuiFlexItem>
-
-          <EuiFlexItem>
-            <EuiTextArea
-              id="markdownVisInput"
-              className="eui-fullHeight"
-              value={card.cardDescription}
-              onChange={({ target: { value } }) => {
-                updateCard(index, { ...card, cardDescription: value });
+          </EuiFormRow>
+          {/* Description of this Drilldown */}
+          <EuiFormRow label="Description">
+            <EuiTextArea name="first" />
+          </EuiFormRow>
+          {/* Action radio buttons */}
+          <EuiFormRow>
+            <EuiRadioGroup
+              options={radios}
+              idSelected={radioIdSelected}
+              onChange={(id) => radioChange(id)}
+              name="radio group"
+              legend={{
+                children: <span>Action</span>,
               }}
-              fullWidth={true}
-              data-test-subj="markdownTextarea"
             />
-          </EuiFlexItem>
-
-          <EuiFlexItem>
-            <EuiTitle size="xs">
-              <h2>
-                <label
-                  htmlFor={i18n.translate('drilldownVisInput', {
-                    defaultMessage: 'Drilldown Visualisation Url',
-                  })}
-                >
-                  Url
-                </label>
-              </h2>
-            </EuiTitle>
-          </EuiFlexItem>
-
-          <EuiFlexItem>
-            <EuiFieldText
-              id="drilldownVisInput"
-              placeholder=""
-              className="eui-fullHeight"
-              value={card.cardUrl}
-              onChange={({ target: { value } }) => {
-                updateCard(index, { ...card, cardUrl: value });
-              }}
+          </EuiFormRow>
+          {/* Select the destination */}
+          <EuiFormRow label="Select a destination">
+            <EuiFieldText name="first" />
+          </EuiFormRow>
+          <EuiFlexGroup direction="column" gutterSize="m" className="eui-fullHeight">
+            <EuiFlexItem>
+              <EuiTitle size="xs">
+                <h2>
+                  <label
+                    htmlFor={i18n.translate('drilldownVisInput', {
+                      defaultMessage: 'Drilldown Visualisation CardName',
+                    })}
+                  >
+                    Card Name
+                  </label>
+                </h2>
+              </EuiTitle>
+            </EuiFlexItem>
+            <EuiFlexItem>
+              <EuiFieldText
+                id="drilldownVisInput"
+                className="eui-fullHeight"
+                value={card.cardName}
+                onChange={({ target: { value } }) => {
+                  updateCard(index, { ...card, cardName: value });
+                }}
+                fullWidth={true}
+              />
+            </EuiFlexItem>
+            <EuiFlexItem>
+              <EuiTitle size="xs">
+                <h2>
+                  <label
+                    htmlFor={i18n.translate('drilldownVisInput', {
+                      defaultMessage: 'Drilldown Visualisation Description',
+                    })}
+                  >
+                    Description
+                  </label>
+                </h2>
+              </EuiTitle>
+            </EuiFlexItem>
+            <EuiFlexItem>
+              <EuiTextArea
+                id="markdownVisInput"
+                className="eui-fullHeight"
+                value={card.cardDescription}
+                onChange={({ target: { value } }) => {
+                  updateCard(index, { ...card, cardDescription: value });
+                }}
+                fullWidth={true}
+                data-test-subj="markdownTextarea"
+              />
+            </EuiFlexItem>
+            <EuiFlexItem>
+              <EuiTitle size="xs">
+                <h2>
+                  <label
+                    htmlFor={i18n.translate('drilldownVisInput', {
+                      defaultMessage: 'Drilldown Visualisation Url',
+                    })}
+                  >
+                    Url
+                  </label>
+                </h2>
+              </EuiTitle>
+            </EuiFlexItem>
+            <EuiFlexItem>
+              <EuiFieldText
+                id="drilldownVisInput"
+                placeholder=""
+                className="eui-fullHeight"
+                value={card.cardUrl}
+                onChange={({ target: { value } }) => {
+                  updateCard(index, { ...card, cardUrl: value });
+                }}
+                fullWidth={true}
+              />
+            </EuiFlexItem>
+            <EuiFlexItem>
+              <EuiTitle size="xs">
+                <h2>
+                  <label
+                    htmlFor={i18n.translate('drilldownVisInput', {
+                      defaultMessage: 'Drilldown Visualisation Destination',
+                    })}
+                  >
+                    Select a Destination
+                  </label>
+                </h2>
+              </EuiTitle>
+            </EuiFlexItem>
+            <EuiSuperSelect
+              options={options}
+              valueOfSelected={valueOfSelected}
+              onChange={onChange}
               fullWidth={true}
+              data-test-subj="chartPicker"
             />
-          </EuiFlexItem>
-
-          <EuiFlexItem>
-            <EuiTitle size="xs">
-              <h2>
-                <label
-                  htmlFor={i18n.translate('drilldownVisInput', {
-                    defaultMessage: 'Drilldown Visualisation Destination',
-                  })}
-                >
-                  Select a Destination
-                </label>
-              </h2>
-            </EuiTitle>
-          </EuiFlexItem>
-
-          <EuiSuperSelect
-            options={options}
-            valueOfSelected={valueOfSelected}
-            onChange={onChange}
-            fullWidth={true}
-            data-test-subj="chartPicker"
-          />
-        </EuiFlexGroup>
+          </EuiFlexGroup>
+        </EuiForm>
       </EuiPanel>
     </EuiAccordion>
   );
