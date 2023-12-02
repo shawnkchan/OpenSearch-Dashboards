bf26be018e0be5ff86d6a8d031c2174d738d82d8
Willie Hung <willie880201044@gmail.com>
2023-12-01 18:57:24 -0600

    Update src/plugins/vis_type_drilldown/public/components/card_form.tsx
    Signed-off-by: Willie Hung <willie880201044@gmail.com>

--- src/plugins/vis_type_drilldown/public/components/card_form.tsx
+++ src/plugins/vis_type_drilldown/public/components/card_form.tsx
@@ -128,7 +128,7 @@ const CardForm = ({
               <h2>
                 <label
                   htmlFor={i18n.translate('drilldownVisInput', {
-                    defaultMessage: 'Drilldown Visualisation Input',
+                    defaultMessage: 'Drilldown Visualisation Destination',
                   })}
                 >
                   Select a Destination
